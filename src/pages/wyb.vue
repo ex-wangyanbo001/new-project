8cca7cc141ac1c582d941cf5b7719a992a92aa5e
wyb17665378291@163.com <wyb12141917>
2020-04-03 20:19:27 +0800

    4月3日

--- src/pages/wyb.vue
+++ src/pages/wyb.vue
@@ -1,22 +1,230 @@
 <template>
-    <div>
+    <div class="pages">
         <div>
             老虎不吃人
+        </div>
+        <div ref='datas' class="float" :class='isSelect?"float":(sliding?"floats":"floatt")' v-show="float">
+            <div @click="Floats">
+                <div class="float-title">i康宝.重疾</div>
+                <div class="float-cneter">
+                    <div class="float-text">热卖</div><div class="float-describe">门诊+住院最高报销200万</div>
+                </div>
+            </div>
+            
+            <div class="float-buttom" @click="floatHidden">></div>
+        </div>
+
+        <div class="insurance">
+            <div class="insurance-top">
+                <div class="img"></div>
+                <div class="insurance-information">
+                    <div class="text"><div>热卖</div><div class='information-title'>i康宝.重疾</div></div>
+                    <div class="text"><div>特色</div><div class="information-title">门诊+住院最高报销400万</div></div>
+                </div>
+            </div>
+            <div class="insurance-buttom">
+                <div class="money-left"><span>低至￥<span>{{20}}</span>元起</span></div>
+                <div class="click-buttom" @click="buttoms">立即获取保障></div>
+            </div>
         </div>
     </div>
 </template>
 <script>
+
 export default {
     data(){
         return{
-
+            float:false,
+            isSelect:true,
+            sliding:'',
+            timer: null,
+            scrollT: 0,
+            stopscroll: 0
+            
         }
     },
+    created(){
+        
+        this.float=true
+    },
+    mounted(){
+         
+        window.addEventListener('scroll',this.handleScroll
+        //  ()=>{console.log('滚动中')}
+        , false);
+    },
+    // destroyed(){
+    //     window.removeEventListener('scroll',this.handleScroll, false);
+    // },
+    
     methods:{
+        
+        floatHidden(){
+            this.float=false
+            
+        },
+        Floats(){
+            this.$router.push({path:'/yh'})
 
+        },
+        
+        handleScroll(){
+           this.isSelect=false
+           console.log(this.$refs.datas)
+            this.sliding=true
+            clearTimeout(this.timer);//清除定時器
+            this.timer = setTimeout(this.ScrollEnd,1000);
+
+           this.scrollT = document.documentElement.scrollTop || document.body.scrollTop;
+        },
+        ScrollEnd() {
+            this.stopscroll = document.documentElement.scrollTop || document.body.scrollTop;//獲取滾動停止后的scrolltop
+            if (this.stopscroll == this.scrollT) {
+                console.log("滚动已结束");
+                this.isSelect=false
+                this.sliding=false
+            }
+        },
+
+        // 点击返回按钮返回上一页面
+        // this.$router.go(-1)
+
+        buttoms(){
+            //点击立即获取保障，跳转对应保险详情页
+        }
     }
 }
 </script>
 <style lang="less" scoped>
+        .pages{
+            width: 750px;
+            height:120vh;
+            
+            position: relative;
+            .floats {
+               
+             animation: move 5s;   
+            @keyframes move{
+				from {
+                        opacity: 1;
+                    }
+                    to {
+                        opacity: 0;
+                    }
+                }
+            }
+           .floatt{
+                animation: moves 5s;
+            @keyframes moves{
+				from {
+                        opacity: 0;
+                    }
+                    to {
+                        opacity: 1;
+                    }
+                }
+           }
+			
 
+           .float{
+               color: white;
+               font-size: 26px;
+               position: absolute;
+               top: 200px;
+               right: 0;
+               width: 450px;
+               height: 140px;
+               background: #cccccc;
+               border-radius: 55px 0 0 55px;
+               display: flex;
+               justify-content: space-around;
+               align-items: center;
+            
+               .float-buttom{
+                   font-size: 45px;
+               }
+               
+               .float-buttom{
+                   width: 50px;
+                   height:100px;
+                   text-align: center;
+                   line-height: 100px;
+
+               }
+           } 
+            .float-cneter{
+                   display: flex;
+                   padding-top: 20px;
+                   box-sizing: border-box;
+                   .float-text{
+                       margin-left: 10px;
+                       background:#cccccc;
+                   }
+                   .float-describe{
+                       padding-left: 10px;
+                        box-sizing: border-box;
+
+                   }
+               }
+           .insurance{
+               width: 720px;
+               height: 400px;
+               margin-top: 400px;
+               margin-left: 15px;
+               margin-right: 15px;
+               border: 1px solid black;
+               border-radius: 5px;
+             .insurance-top{
+                 padding-top: 20px;
+                 padding-left: 20px;
+                 display: flex;
+                 
+                 .img{
+                     
+                     width: 300px;
+                     height: 200px;
+                     border: 1px solid black;
+                 }
+                 .insurance-information{
+                     padding-top: 20px;
+                    //  display: flex;
+                    flex: 1;
+                    height: 200px;
+                    .text{
+                        display: flex;
+                        padding-left: 20px;
+                        
+                        .information-title{
+                            padding-left: 30px;
+                            // line-height: 100px;
+                        }
+                    }
+                 }
+             }
+             .insurance-buttom{
+                 padding-top: 100px;
+                //  width: 200px;
+                height: 60px;
+                 display: flex;
+                 justify-content: space-around;
+                 .money-left{
+                     line-height: 60px;
+                     margin-left: -90px;
+                 }
+                 .click-buttom{
+                     width: 200px;
+                     height: 60px;
+                     line-height: 60px;
+                     text-align: center;
+                     border: 1px solid black;
+                     border-radius: 25px;
+                     margin-right: -80px;
+                 }
+             }
+           }
+           .insurance-information :nth-child(1){
+            //    padding-top: 10px;
+               padding-bottom: 50px;
+           }
+        }
 </style>
--- src/pages/yh.vue
+++ src/pages/yh.vue
@@ -1,24 +1,176 @@
 <template>
-<div>
-    <div>12345</div>
-    <div>上山打老虎</div>
+<div class="pages">
+    <div class="news-index">
+            <div class="news-tabs">
+                <ul>
+                  <li :class="isSelect==index? ' tabs ':'tabs2'" v-for="(item,index) in list" :key="index" @click="Tabs(index)">{{item}}</li>
+                </ul>
+             </div>
+
+            <div class="news-project">
+                <div class="scroll-div">
+                    <div  class="project-item" v-for="(item1,index1) in lists" :key="index1">{{item1}}</div>
+                </div>
+             </div>   
+    </div>
+    <div class="card-list" >
+        <div class="card-center">
+            <div class="pk">pk</div>
+         <div class="card-title">{{title}}</div>
+         <div class="vote">
+             <div class="vote-yes">有</div><div class="vote-no">没有</div>
+         </div>
+        </div>
+    </div>
 </div>
     
 </template>
 
 <script>
+import { Swiper } from 'vux'
 export default {
+    components: {
+    Swiper
+  },
     data(){
         return{
-
+            isSelect:0,
+            list:['推荐','保险','健康','视频'],
+            lists:['小白必看','保险推荐','理赔宝典','防坑指南','嘘嘘嘘嘘','哈哈哈哈'],
+            title:'宅在家有没有更轻松？投票权交给你会投给哪个一票呢？?????????????????????'
+           
         }
     },
+    created(){
+        
+    },
+    mounted(){
+       
+    },
     methods:{
+        Tabs(index){
+            this.isSelect=index
+        },
         
     }
 }
 </script>
 
 <style lang="less" scoped>
-
+ .pages{
+     width: 750px;
+     height: 100vh;
+     background-color: #cccccc;
+     .news-index{
+         width: 100%;
+         height: 300px;
+         background-color: #ffffff;
+         border-top: 1px solid #cccccc;
+        .news-tabs{
+            width: 100%;
+            height: 60px;
+            padding-top: 20px;
+            padding-left: 20px;
+            padding-bottom: 80px;
+            box-sizing: border-box;
+            ul{
+                display: flex;
+            
+             .tabs{
+                 color: black;
+                 font-size: 36px;
+                 padding-right: 20px;
+                 position: relative;
+             &:after {
+                
+              content: '';
+              position: absolute;
+              bottom: -15px;
+              top: auto;
+              left: 25px;
+              height: 3px;
+              width: 22px;
+              background-color: black;
+            }
+             }
+            .tabs2{
+                width: 120px;
+                color: gray;
+                font-size: 28px;
+                padding-right: 20px;
+                 
+               
+            }
+            }
+         } 
+       .news-project{
+           width: 100%;
+           padding-left: 20px;
+           height: 200px;
+            overflow: scroll;
+           
+        // display: flex;
+        .scroll-div{
+            width: 200%;
+            display: flex;
+             }
+         .project-item{
+            margin-right: 30px;
+            width: 200px;
+            height: 150px;
+            border-radius: 10px;
+            border: 1px solid #cccccc;
+            text-align: center;
+            line-height: 150px;
+            font-size: 40px;
+           }
+       }  
+       .news-project::-webkit-scrollbar {display:none} 
+     }
+ .card-list{
+    width: 100%;
+    height: 500px;
+    background: #ffffff;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    padding: 20px ;
+    .card-center{
+        width: 710px;
+        height: 100%;
+        border: 1px solid gray;
+        border-radius: 10px;
+        text-align: center;
+        
+        .pk{
+            padding-top: 40px;
+            padding-bottom: 40px;
+            font-size: 60px;
+            color: black;
+        }
+        .card-title{
+            width: 100%;
+            height: 120px;
+            line-height: 40px;
+            font-size: 36px;
+            overflow: hidden;
+            -webkit-line-clamp:2;//显示行数
+            text-overflow:ellipsis;//显示为省略号
+        }
+        .vote{
+            width: 100%;
+            display: flex;
+            text-align: center;
+            .vote-yes{
+                width: 120px;
+                height: 60px;
+            }
+            .vote-no{
+                width: 120px;
+                height: 60px;
+            }
+            
+        }
+    }
+ }
+ }
 </style>
